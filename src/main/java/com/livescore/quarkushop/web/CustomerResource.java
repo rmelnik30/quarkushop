e55b3999283a5d416bae0f27b057dbb2ff63d27c
rmelnik30 <melnik.roman@gmail.com>
2023-02-04 12:07:57 +0200

    web layer

---
+++ src/main/java/com/livescore/quarkushop/web/CustomerResource.java
@@ -0,0 +1,59 @@
+package com.livescore.quarkushop.web;
+
+import com.livescore.quarkushop.service.CustomerService;
+import com.livescore.quarkushop.web.dto.CustomerDto;
+import io.quarkus.security.Authenticated;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+/**
+ * @author Nebrass Lamouchi
+ */
+
+@Path("/customers")
+@Authenticated
+@Tag(name = "customer", description = "All the customer methods")
+public class CustomerResource {
+
+    @Inject
+    CustomerService customerService;
+
+    @GET
+    public List<CustomerDto> findAll() {
+        return this.customerService.findAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    public CustomerDto findById(@PathParam("id") Long id) {
+        return this.customerService.findById(id);
+    }
+
+    @GET
+    @Path("/active")
+    public List<CustomerDto> findAllActive() {
+        return this.customerService.findAllActive();
+    }
+
+    @GET
+    @Path("/inactive")
+    public List<CustomerDto> findAllInactive() {
+        return this.customerService.findAllInactive();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public CustomerDto create(CustomerDto customerDto) {
+        return this.customerService.create(customerDto);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void delete(@PathParam("id") Long id) {
+        this.customerService.delete(id);
+    }
+}
